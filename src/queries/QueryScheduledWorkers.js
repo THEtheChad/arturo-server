9b7a00f2c071b7d3cfa96e1b096a2f1659945178
THEtheChad <chad.d.elliott@gmail.com>
2020-11-13 10:35:27 -0600

    updated custom query

--- src/queries/QueryScheduledWorkers.js
+++ src/queries/QueryScheduledWorkers.js
@@ -1,34 +1,33 @@
-import Debug from 'debug'
-import stream from 'stream'
-import uuid from '../utilities/uuid'
-import Shutdown from '../server/Shutdown'
+import Debug from 'debug';
+import stream from 'stream';
+import uuid from '../utilities/uuid';
+import Shutdown from '../server/Shutdown';
 
 export default class QueryScheduledWorkers extends stream.Readable {
-  constructor(sequelize) {
-    super({
-      highWaterMark: 0,
-      objectMode: true
-    })
+	constructor(sequelize) {
+		super({
+			highWaterMark: 0,
+			objectMode: true
+		});
 
-    this.sequelize = sequelize
-    this.uuid = `${this.constructor.name}-${process.pid}-${uuid()}`
-    this.debug = Debug(`arturo:${this.uuid}`)
+		this.sequelize = sequelize;
+		this.uuid = `${this.constructor.name}-${process.pid}-${uuid()}`;
+		this.debug = Debug(`arturo:${this.uuid}`);
 
-    Shutdown.addHandler(
-      (code, sig, done) => this.destroy(null, done),
-      (remove) => this.once('end', remove)
-    )
-  }
+		Shutdown.addHandler(
+			(code, sig, done) => this.destroy(null, done),
+			(remove) => this.once('end', remove)
+		);
+	}
 
-  fetch() {
-    if (this.destroyed) return
-    if (this.readableLength > 0) return
+	fetch() {
+		if (this.destroyed) return;
+		if (this.readableLength > 0) return;
 
-    const { Op, dialect } = this.sequelize
-    const generator = dialect.QueryGenerator
-
-    // @TODO: limit to one job route
-    this.sequelize.query(`
+		// @TODO: limit to one job route
+		this.sequelize
+			.query(
+				`
       SELECT *
       FROM Workers
       WHERE
@@ -39,23 +38,24 @@ export default class QueryScheduledWorkers extends stream.Readable {
           FROM Jobs
           WHERE
               Jobs.status IN ('scheduled', 'retry') AND
-              ${generator.whereItemQuery(this.sequelize.literal('Jobs.scheduledDate'), { [Op.lte]: new Date })}
+              Jobs.scheduledDate <= Convert(datetime, '${new Date()}' )
             GROUP BY Jobs.route
-        )
-    `)
-      .spread((workers, metadata) => {
-        this.debug(`found ${workers.length} worker(s) with scheduled jobs...`)
-        workers.forEach(worker => {
-          if (!this.destroyed) this.push(worker)
-        })
-      })
-  }
+        )j
+    `
+			)
+			.spread((workers, metadata) => {
+				this.debug(`found ${workers.length} worker(s) with scheduled jobs...`);
+				workers.forEach((worker) => {
+					if (!this.destroyed) this.push(worker);
+				});
+			});
+	}
 
-  _read() { }
+	_read() {}
 
-  _destroy(err, done) {
-    this.push(null)
-    this.debug('DESTROYED')
-    done(err)
-  }
+	_destroy(err, done) {
+		this.push(null);
+		this.debug('DESTROYED');
+		done(err);
+	}
 }
